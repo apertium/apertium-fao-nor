881803be9103052e623d3dd61da990cd0b29b837
Francis M. Tyers <ftyers@prompsit.com>
2009-10-07 12:42:11 +0000

    fix script

--- dev/bidix/rerun-scores.sh
+++ dev/bidix/rerun-scores.sh
@@ -2,7 +2,7 @@
 #
 # Regenerate scored files, after changing calc-levenshtein.py
 
-cat noun.list | python calc-levenshtein.py   | sort -gr > noun.scored.list
+cat noun.list | grep -v '<Prop>' | python calc-levenshtein.py   | sort -gr > noun.scored.list
 cat vblex.list | python calc-levenshtein.py | sort -gr > vblex.scored.list 
 cat adv.list | python calc-levenshtein.py | sort -gr > adv.scored.list
 cat prep.list | python calc-levenshtein.py | sort -gr > prep.scored.list
