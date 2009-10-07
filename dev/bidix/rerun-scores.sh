cd2d1a29c05981d2988552d4a001c19b14c23201
Francis M. Tyers <ftyers@prompsit.com>
2009-10-07 12:41:45 +0000

    add bidix stuff for fo-nb

---
+++ dev/bidix/rerun-scores.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Regenerate scored files, after changing calc-levenshtein.py
+
+cat noun.list | python calc-levenshtein.py   | sort -gr > noun.scored.list
+cat vblex.list | python calc-levenshtein.py | sort -gr > vblex.scored.list 
+cat adv.list | python calc-levenshtein.py | sort -gr > adv.scored.list
+cat prep.list | python calc-levenshtein.py | sort -gr > prep.scored.list
+cat adj.list | python calc-levenshtein.py | sort -gr > adj.scored.list
