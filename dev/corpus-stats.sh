af345d46e498af7fe37e2801453935e078e31902
Francis M. Tyers <ftyers@prompsit.com>
2017-07-12 11:18:14 +0000

    ugh

--- dev/corpus-stats.sh
+++ dev/corpus-stats.sh
@@ -2,13 +2,13 @@ GSED=`which gsed`;
 if [[ "$GSED" == "" ]]; then
 	GSED=`which sed`;
 fi
+
 GECHO=`which gecho`;
 if [[ "$GECHO" == "" ]]; then
 	GECHO=`which echo`;
 fi
 
-
-lang1path=`cat config.log | grep lang1= | $GSED 's/with-lang[^=]\+=/@/g' | cut -f2 -d'@' | sed 's/--//g' | sed 's/ *//g'`
+lang1path=`cat config.log | grep lang1= | $GSED 's/with-lang[^=]\+=/@/g' | cut -f2 -d'@' | $GSED 's/--//g' | $GSED 's/ *//g'`
 lang1=fao
 lang2path=`cat config.log | grep lang2b= | $GSED 's/with-lang[^=]\+=/@/g' | cut -f4 -d'@'`
 lang2=`basename $lang2path | $GSED 's/apertium-//g'`
@@ -16,14 +16,14 @@ lang2=`basename $lang2path | $GSED 's/apertium-//g'`
 pair=fao-nob
 
 tee /tmp/$pair.input | apertium -d . $pair-postchunk 2>/dev/null > /tmp/$pair.postchunk2.0
-cat /tmp/$pair.postchunk2.0 | $GSED 's/\$/$\n/g' | sed 's/^ *//g' | sed 's/ *$//g' | grep -v '^ *$' > /tmp/$pair.postchunk2.1
+cat /tmp/$pair.postchunk2.0 | $GSED 's/\$/$\n/g' | $GSED 's/^ *//g' | $GSED 's/ *$//g' | grep -v '^ *$' > /tmp/$pair.postchunk2.1
 cat /tmp/$pair.postchunk2.1 | lt-proc -g $pair.autogen.bin > /tmp/$pair.gen
 
 total=`cat /tmp/$pair.postchunk2.1 | wc -l`
 generr=`cat /tmp/$pair.gen | grep '#' | wc -l`;
 unkerr=`cat /tmp/$pair.postchunk2.1 | grep '\*' | wc -l`;
 
-cat /tmp/$pair.input | apertium-destxt | hfst-proc -w "$lang1path/analyser-mt-apertium-desc.und.hfstol" | apertium-retxt | $GSED 's/\$/$\n/g' | sed 's/^ *//g' | sed 's/ *$//g' | grep -v '^ *$' > /tmp/$pair.morph
+cat /tmp/$pair.input | apertium-destxt | hfst-proc -w "$lang1path/analyser-mt-apertium-desc.und.hfstol" | apertium-retxt | $GSED 's/\$/$\n/g' | $GSED 's/^ *//g' | $GSED 's/ *$//g' | grep -v '^ *$' > /tmp/$pair.morph
 
 untr_total=`cat /tmp/$pair.morph | wc -l`;
 untr_known=`cat /tmp/$pair.morph | grep -v '\*' | wc -l`;
