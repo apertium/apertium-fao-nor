a9f93f7441ba17e415b5d7f3e9541ec9f3e1fca2
Francis M. Tyers <ftyers@prompsit.com>
2017-08-11 12:49:10 +0000

    remove old dir

--- dev/bidix/rerun-scores.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Regenerate scored files, after changing calc-levenshtein.py
-
-cat noun.list | grep -v '<Prop>' | python calc-levenshtein.py   | sort -gr > noun.scored.list
-cat vblex.list | python calc-levenshtein.py | sort -gr > vblex.scored.list 
-cat adv.list | python calc-levenshtein.py | sort -gr > adv.scored.list
-cat prep.list | python calc-levenshtein.py | sort -gr > prep.scored.list
-cat adj.list | python calc-levenshtein.py | sort -gr > adj.scored.list
